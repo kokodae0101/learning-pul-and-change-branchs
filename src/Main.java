3c1569e2d0de70bfe32004fa98a55fbfc2794bad
dastanchick <dosyafree32@gmail.com>
2022-11-13 14:15:47 +0600

    I am added 1-25

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
+        System.out.println("I'm Dastan");
         System.out.println("agferga");
         System.out.println("agferga");
         System.out.println("agferga");
